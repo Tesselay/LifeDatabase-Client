198ca9f81a683bc59cb751361af0af898249d458
Lahl, Dominique <47154137+Tesselay@users.noreply.github.com>
2025-01-12 17:39:16 +0100

    Add navbar with auto generated tabs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Router, RouterLink, RouterOutlet, Routes} from '@angular/router';
+import {MatToolbar} from '@angular/material/toolbar';
+import {CommonModule} from '@angular/common';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, CommonModule, MatToolbar, MatButton],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Client';
+  title = 'LifeDatabase Client';
+  routes: Routes;
+  console: any = console;
+
+  constructor(private router: Router) {
+    this.routes = this.router.config;
+    this.routes = this.router.config.filter(route => !route.redirectTo);
+  }
 }
