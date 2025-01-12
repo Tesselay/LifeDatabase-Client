621635e4e4703f3cb34b2d48c20caa664cb9a9c0
Lahl, Dominique <47154137+Tesselay@users.noreply.github.com>
2025-01-12 17:42:26 +0100

    Add service for article querying

---
+++ src/app/shared/dataservices/article.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {environment} from '../../../resources/environment';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Page} from '../dataobjects/page';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArticleService {
+
+  private url = environment.backendUrl;
+
+  constructor(private http: HttpClient) { }
+
+  getArticles(params: HttpParams) {
+    return this.http.get<Page>(this.url + '/articles', {params: params});
+  }
+}
